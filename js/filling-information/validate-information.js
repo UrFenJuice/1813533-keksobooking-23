d3fc4397e11cd841aeb55018858fda3147949a9c
Yuri Shuin <wuin69@gmail.com>
2021-06-22 17:38:58 +0300

    add validate functions

--- js/filling-information/validate-information.js
+++ js/filling-information/validate-information.js
@@ -1,5 +1,97 @@
-const validateInfo = function () {
+const form = document.querySelector('.ad-form');
+const title = form.querySelector('#title');
+const price = form.querySelector('#price');
+const roomNumber = form.querySelector('#room_number');
+const capacity = form.querySelector('#capacity');
 
+const MIN_TITLE_LENGTH = title.getAttribute('minlength');
+const MAX_TITLE_LENGTH = title.getAttribute('maxlength');
+const MAX_PRICE = price.getAttribute('max');
+
+const titleValidate = function () {
+  const valueLength = title.value.length;
+
+  if (valueLength < MIN_TITLE_LENGTH) {
+    title.setCustomValidity(`Ещё ${  MIN_TITLE_LENGTH - valueLength } симв.`);
+  } else if (valueLength > MAX_TITLE_LENGTH) {
+    title.setCustomValidity(`Удалите лишние ${  valueLength - MAX_TITLE_LENGTH } симв.`);
+  } else {
+    title.setCustomValidity('');
+  }
+
+  title.reportValidity();
 };
 
-export {validateInfo};
+title.addEventListener('input', titleValidate);
+
+const priceValidate = function () {
+  if (+price.value > +MAX_PRICE) {
+    price.setCustomValidity(`Цена за ночь не должна превышать ${  MAX_PRICE }`);
+  } else {
+    price.setCustomValidity('');
+  }
+
+  price.reportValidity();
+};
+
+price.addEventListener('input', priceValidate);
+
+const roomNumberChangeHandler = function (evt) {
+  const targetElement = evt.target;
+
+  if (capacity.value !== targetElement.value) {
+    if (+targetElement.value > 99) {
+      targetElement.setCustomValidity(`Эта недвижимость ${ capacity.querySelector('option[value="0"]').textContent }`);
+    } else {
+      targetElement.setCustomValidity(`Эта недвижимость ${ capacity.querySelector(`option[value="${ targetElement.value }"]`).textContent }`);
+    }
+  } else {
+    targetElement.setCustomValidity('');
+  }
+
+  targetElement.reportValidity();
+};
+
+roomNumber.addEventListener('change', roomNumberChangeHandler);
+
+const roomNumberValidate = function () {
+  const targetElement = roomNumber;
+
+  if (capacity.value !== targetElement.value) {
+    if (+targetElement.value > 99) {
+      targetElement.setCustomValidity(`Эта недвижимость ${ capacity.querySelector('option[value="0"]').textContent }`);
+    } else {
+      targetElement.setCustomValidity(`Эта недвижимость ${ capacity.querySelector(`option[value="${ targetElement.value }"]`).textContent }`);
+    }
+  } else {
+    targetElement.setCustomValidity('');
+  }
+  targetElement.reportValidity();
+};
+
+const capacityChangeHandler = function (evt) {
+  const targetElement = evt.target;
+
+  if (roomNumber.value !== targetElement.value) {
+    if (+targetElement.value < 1) {
+      targetElement.setCustomValidity(`${ targetElement.options[targetElement.selectedIndex].textContent } ${ roomNumber.querySelector('option[value="100"]').textContent }`);
+    } else {
+      targetElement.setCustomValidity(`${ targetElement.options[targetElement.selectedIndex].textContent } ${ roomNumber.querySelector(`option[value="${ targetElement.value }"]`).textContent }`);
+    }
+  } else {
+    targetElement.setCustomValidity('');
+  }
+
+  targetElement.reportValidity();
+};
+
+capacity.addEventListener('change', capacityChangeHandler);
+
+
+const formChangeHandler = function () {
+  titleValidate();
+  priceValidate();
+  roomNumberValidate();
+};
+
+form.addEventListener('submit', formChangeHandler);
--- js/main.js
+++ js/main.js
@@ -6,7 +6,7 @@ import {activatedApp} from './state/activated-app.js';
 
 import {fillingInfo} from './filling-information/filling-information.js';
 import {publishInfo} from './filling-information/publish-information.js';
-import {validateInfo} from './filling-information/validate-information.js';
+import './filling-information/validate-information.js';
 import {confirmedInfo} from './filling-information/confirmed-information.js';
 import {showMessage} from './filling-information/show-message.js';
 import {showErrorMessage} from './filling-information/show-error-message.js';
@@ -24,8 +24,6 @@ activatedApp();
 fillingInfo();
 
 publishInfo();
-
-validateInfo();
 
 confirmedInfo();
 
