d3bd5a1e4253f5e7aeaf57b4b33c8d622e272c5b
Yuri Shuin <wuin69@gmail.com>
2021-07-05 10:58:59 +0300

    update resetForm function

--- js/filling-information/reset-form.js
+++ js/filling-information/reset-form.js
@@ -1,31 +1,28 @@
 import {defaultLat, defaultLng, address, markerGroup, mainPinIcon} from '../utils/create-map.js';
 
-const resetForm = () => {
-  const reset = new Promise((resolve) => {
-    document.querySelector('.ad-form').reset();
-    document.querySelector('.map__filters').reset();
+const resetForm = (event) => {
+  event.preventDefault();
+  document.querySelector('.ad-form').reset();
+  document.querySelector('.map__filters').reset();
+  markerGroup.clearLayers();
 
-    resolve(markerGroup.clearLayers());
-  });
-  reset.then(() => {
-    const mainPinMarker = L.marker(
-      {
-        lat: defaultLat,
-        lng: defaultLng,
-      },
-      {
-        draggable: true,
-        icon: mainPinIcon,
-      },
-    );
-    mainPinMarker.addTo(markerGroup);
+  const mainPinMarker = L.marker(
+    {
+      lat: defaultLat,
+      lng: defaultLng,
+    },
+    {
+      draggable: true,
+      icon: mainPinIcon,
+    },
+  );
+  mainPinMarker.addTo(markerGroup);
 
-    address.value = `${ defaultLat } , ${ defaultLng }`;
+  address.value = `${ defaultLat } , ${ defaultLng }`;
 
-    mainPinMarker.on('dragend',(evt)=> {
-      const chagedPos = evt.target.getLatLng();
-      address.value = `${ chagedPos.lat.toFixed(5) }, ${ chagedPos.lng.toFixed(5) }`;
-    });
+  mainPinMarker.on('dragend',(evt)=> {
+    const chagedPos = evt.target.getLatLng();
+    address.value = `${ chagedPos.lat.toFixed(5) }, ${ chagedPos.lng.toFixed(5) }`;
   });
 };
 
